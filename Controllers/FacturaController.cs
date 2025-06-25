5020501afe5f1cced0f1e01b6497a3f8c745c913
JeffersonGl95 <138807765+JeffersonGl95@users.noreply.github.com>
2025-06-25 15:50:45 -0500

    Validaciones

--- Controllers/FacturaController.cs
+++ Controllers/FacturaController.cs
@@ -25,20 +25,40 @@ namespace FacturaApi.Controllers
         [HttpPost]
         public async Task<ActionResult> Post([FromBody] List<Factura> facturas)
         {
-            foreach (var f in facturas)
+            try
             {
-                await _service.Crear(f);
-            }
+                foreach (var f in facturas)
+                {
+                    await _service.Crear(f);
+                }
 
-            return Ok(new { total = facturas.Count });
+                return Ok(new { total = facturas.Count });
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(new { response = "{ \r\n \"state\": false, \r\n \"data\": null,\r\n \"code\": \"400\",\r\n \"message\": " + ex + "} \r\n} " });
+                throw;
+            }
+           
         }
 
         [HttpPut("{id}")]
         public async Task<ActionResult> Put(int id, [FromBody] Factura factura)
         {
-            if (id != factura.Id) return BadRequest();
-            await _service.Actualizar(factura);
-            return NoContent();
+            try
+            {
+                if (id != factura.Id) return BadRequest();
+                await _service.Actualizar(factura);
+                return NoContent();
+
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(new { response = "{ \r\n \"state\": false, \r\n \"data\": null,\r\n \"code\": \"400\",\r\n \"message\": " + ex + "} \r\n} " });
+                throw;
+            }
+
+           
         }
     }
 }
